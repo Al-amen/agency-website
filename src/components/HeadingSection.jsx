f994f4067275351d1d109d498f47653568dc5b8f
Al-Amen <alamen15@cse.pstu.ac.bd>
2025-06-24 14:46:36 +0600

    implement services

--- src/components/HeadingSection.jsx
+++ src/components/HeadingSection.jsx
@@ -4,7 +4,7 @@ const HeadingSection = ({heading,subHeading,description}) => {
   return (
      <div className='text-center mb-12'>
           <p className='text-blue-600 font-semibold uppercase'>{subHeading}</p>
-          <h2 className='text-3xl lg:text-4xl font-bold text-gray-800 mt-3'>{heading}</h2>
+          <h2 className='text-3xl lg:text-4xl font-bold text-gray-800 mt-3 sm:w-3/5 leading-normal lg:leading-snug capitalize mx-auto'>{heading}</h2>
 
         {
             description && <p className='text-gray-600 mt-4 sm:w-3/5 text-base mx-auto'>{description}</p>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,12 +3,15 @@ import HeroSection from './HeroSection'
 import ToolsSection from './ToolsSection'
 import CompanyLogo from './CompanyLogo'
 
+import Service from './Service'
+
 const Home = () => {
   return (
     <>
      <HeroSection/>
      <ToolsSection/>
      <CompanyLogo/>
+     <Service/>
     </>
   )
 }
